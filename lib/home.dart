fdf5196907e73b179439bd864182ff356fc12feb
Misbah <misbahmuneer2003@gmail.com>
2025-02-04 20:05:18 +0500

    Finalized

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
@@ -16,27 +17,110 @@ class _BasicAudioPlayerState extends State<BasicAudioPlayer> {
     {
       "title": "Kaisa Mai",
       "path": "audio/KaisaMai.mp3",
-      "image": "assets/images/anv.jpg"
+      "image": "assets/images/arpit.webp",
+      "artist": "Arpit Bala"
     },
     {
       "title": "Nindiya ke Par",
       "path": "audio/Nindiya.mp3",
-      "image": "assets/images/anv.jpg"
+      "image": "assets/images/uzair.jpg",
+      "artist": "Uzair Jaswal"
     },
     {
       "title": "Zimmedar",
       "path": "audio/Zimmedaar.mp3",
-      "image": "assets/images/zim.jpg"
+      "image": "assets/images/arpit.webp",
+      "artist": "Arpit Bala"
     },
     {
       "title": "Gulabo",
       "path": "audio/GULABO.mp3",
-      "image": "assets/images/gul.jpg"
+      "image": "assets/images/arpit.webp",
+      "artist": "Arpit Bala"
     },
     {
       "title": "Last Friday Night",
       "path": "audio/Katy.mp3",
-      "image": "assets/images/last.jpg"
+      "image": "assets/images/kate.jpg",
+      "artist": "Katy Perry"
+    },
+    {
+      "title": "Boy with Luv",
+      "path": "audio/bts.mp3",
+      "image": "assets/images/v.jpg",
+      "artist": "BTS"
+    },
+    {
+      "title": "Dandelions",
+      "path": "audio/dandelions.mp3",
+      "image": "assets/images/dan.jpeg",
+      "artist": "BTS"
+    },
+    {
+      "title": "You're My Escape",
+      "path": "audio/escape.mp3",
+      "image": "assets/images/na.avif",
+      "artist": "Naruto"
+    },
+    {
+      "title": "F U N K A",
+      "path": "audio/funka.mp3",
+      "image": "assets/images/arpit.webp",
+      "artist": "Arpit Bala"
+    },
+    {
+      "title": "Love & Honour",
+      "path": "audio/itachi.mp3",
+      "image": "assets/images/it.jpg",
+      "artist": "Naruto"
+    },
+    {
+      "title": "Without You",
+      "path": "audio/kakashi.mp3",
+      "image": "assets/images/ka.jpeg",
+      "artist": "Naruto"
+    },
+    {
+      "title": "Kabir Singh",
+      "path": "audio/kitna.mp3",
+      "image": "assets/images/sha.jpg",
+      "artist": "Arijit Singh"
+    },
+    {
+      "title": "Lucid Dreams",
+      "path": "audio/Lucid.mp3",
+      "image": "assets/images/de.jpeg",
+      "artist": "Juice"
+    },
+    {
+      "title": "MayaBee",
+      "path": "audio/maya.mp3",
+      "image": "assets/images/maya.jpg",
+      "artist": "Maya"
+    },
+    {
+      "title": "Espresso",
+      "path": "audio/sabrina.mp3",
+      "image": "assets/images/sab.jpg",
+      "artist": "Sabrina Carpenter"
+    },
+    {
+      "title": "Timmy Turner",
+      "path": "audio/tim.mp3",
+      "image": "assets/images/de.jpeg",
+      "artist": "Desiigner"
+    },
+    {
+      "title": "Love & Angel",
+      "path": "audio/tra.mp3",
+      "image": "assets/images/tra.jpg",
+      "artist": "Trafalgar"
+    },
+    {
+      "title": "Tum Hy Ho",
+      "path": "audio/tumhiho.mp3",
+      "image": "assets/images/sha.jpg",
+      "artist": "Arijit Singh"
     },
   ];
 
@@ -46,9 +130,9 @@ class _BasicAudioPlayerState extends State<BasicAudioPlayer> {
 
     _audioPlayer.onPlayerComplete.listen((event) {
       if (isLooping) {
-        _playSong(currentIndex); // Repeat current song
+        _playSong(currentIndex);
       } else {
-        _nextSong(); // Move to next song
+        _nextSong();
       }
     });
   }
@@ -62,6 +146,10 @@ class _BasicAudioPlayerState extends State<BasicAudioPlayer> {
         isPlaying = true;
         currentIndex = index;
       });
+
+      // Debugging print
+      print("🎵 Playing song: ${songs[currentIndex]}");
+      print("🎤 Artist: ${songs[currentIndex]["artist"]}");
     } catch (e) {
       print("❌ Error playing audio: $e");
     }
@@ -97,59 +185,91 @@ class _BasicAudioPlayerState extends State<BasicAudioPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Audio Player")),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Display image for the current song
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                songs[currentIndex]["image"]!,
-                width: 250,
-                height: 250,
-                fit: BoxFit.cover,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Now Playing: ${songs[currentIndex]["title"]}",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.skip_previous_rounded, size: 40),
-                  onPressed: _previousSong,
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: <Color>[Colors.black, Colors.blue]),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CarouselSlider(
+                items: songs.map((song) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(
+                      song["image"]!,
+                      width: 350,
+                      height: 350,
+                      fit: BoxFit.contain,
+                    ),
+                  );
+                }).toList(),
+                options: CarouselOptions(
+                  height: 250,
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: true,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                    _playSong(index);
+                  },
                 ),
-                IconButton(
-                  icon: Icon(
-                    isPlaying ? Icons.pause_rounded : Icons.play_arrow_rounded,
-                    size: 50,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                songs[currentIndex]["title"]!,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                songs[currentIndex]["artist"] ?? "Unknown Artist",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 80),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.skip_previous_rounded, size: 50),
+                    onPressed: _previousSong,
                   ),
-                  onPressed:
-                      isPlaying ? _pauseAudio : () => _playSong(currentIndex),
-                ),
-                IconButton(
-                  icon: Icon(Icons.skip_next_rounded, size: 40),
-                  onPressed: _nextSong,
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            IconButton(
-              icon: Icon(
-                isLooping ? Icons.loop : Icons.repeat_one_rounded,
-                color: isLooping ? Colors.green : Colors.black,
-                size: 30,
+                  IconButton(
+                    icon: Icon(
+                      isPlaying
+                          ? Icons.pause_rounded
+                          : Icons.play_arrow_rounded,
+                      size: 50,
+                    ),
+                    onPressed:
+                        isPlaying ? _pauseAudio : () => _playSong(currentIndex),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.skip_next_rounded, size: 50),
+                    onPressed: _nextSong,
+                  ),
+                ],
               ),
-              onPressed: _toggleLoop,
-            ),
-            Text(isLooping ? "Looping ON" : "Looping OFF"),
-          ],
+              const SizedBox(height: 20),
+              IconButton(
+                icon: Icon(
+                  isLooping ? Icons.loop : Icons.repeat_one_rounded,
+                  color: isLooping ? Colors.green : Colors.black,
+                  size: 30,
+                ),
+                onPressed: _toggleLoop,
+              ),
+              Text(isLooping ? "Looping ON" : "Looping OFF"),
+            ],
+          ),
         ),
       ),
     );
